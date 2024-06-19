c295316254b73ff5c683d8d4fba5f1ad03339509
Artem2145 <artemplotsidim@gmail.com>
2024-06-19 22:51:01 +0300

    timer

--- main.js
+++ main.js
@@ -1,21 +1,54 @@
 let counter = 0;
+let clicksPerMinute = 0;
+const maxClicksPerMinute = 50;
 
 let title = document.createElement("h1");
-title.textContent= `Ти нажал ${counter} раз`;
+title.textContent = `Ти нажал ${counter} раз`;
+
+let level = 1;
+
+let para = document.createElement("p");
+para.textContent = `Уровень ${level}`;
 
 let button = document.createElement("button");
 button.textContent = "Нажать";
 
-
 button.onclick = function () {
   counter++;
   title.textContent = `Ти нажал ${counter} раз`;
-  if (counter >= 150) {
-    title.textContent = `Єбать ти крутой`;
-    document.body.classList.add("new-class");
+  if (clicksPerMinute < maxClicksPerMinute) {
+    clicksPerMinute++;
+    if (counter >= 10) {
+      document.body.classList.add("new-class");
+      level = 2;
+      para.textContent = `Уровень ${level}`;
+    }
+    if (counter >= 30) {
+      level = 3;
+      para.textContent = `Уровень ${level}`;
+    }
+    if (counter >= 50) {
+      level = 4;
+      para.textContent = `Уровень ${level}`;
+    }
+    if (counter >= 100) {
+      level = 5;
+      para.textContent = `Уровень ${level}`;
+    }
+    if (counter >= 150) {
+      level = 6;
+      para.textContent = `Уровень ${level}`;
+    }
+  }
+  else{
+    alert("Вы превысили лимит нажатий в пол минуту!");
   }
 };
 
-
 document.body.append(title);
+document.body.append(para);
 document.body.append(button);
+
+setInterval(() => {
+  clicksPer30Seconds = 0;
+}, 30000);
